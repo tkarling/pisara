12fd5bb9b4d2a77948729e8760e5e39fb0832b49
tkarling <tkarling@live.com>
2017-06-04 17:03:16 -0700

    shows photos from server

--- photo-client/src/App.js
+++ photo-client/src/App.js
@@ -1,34 +1,75 @@
 import React, { Component } from "react";
-import { Image, StyleSheet, View } from "react-native";
+import { StyleSheet, View, Image } from "react-native";
 import "./App.css";
 
 // Components
 const Card = ({ children }) => <View style={styles.card}>{children}</View>;
-const Photo = ({ uri }) => {
+const Photo = ({ src }) => {
   const { innerWidth: width, innerHeight: height } = window;
+  if (!src) {
+    return <view />;
+  }
+  const uri = window.URL.createObjectURL(src);
   return (
     <Image source={{ uri }} style={{ width, height }} resizeMode="contain" />
   );
 };
 
+const getNexPhoto = () => {
+  return fetch("http://10.0.1.8:8080/v2/photo/next", {
+    method: "get",
+    headers: {
+      Accept: "application/json"
+    }
+  })
+    .then(result => result.json())
+    .then(meta => {
+      return fetch("http://10.0.1.8:8080/v2/photo/photo", {
+        method: "get",
+        headers: {
+          Accept: "image/*",
+          URL: meta.URL
+        }
+      })
+        .then(myBlob => myBlob.blob())
+        .then(myBlob => {
+          return myBlob;
+        });
+    });
+};
+
 class App extends Component {
+  timer = null;
+  state = {
+    photo: null
+  };
   // resize based on https://stackoverflow.com/questions/19014250/reactjs-rerender-on-browser-resize
   updateDimensions = () => {
     this.setState({});
   };
+  updatePhoto = () => {
+    getNexPhoto().then(photo => {
+      this.setState({ photo });
+    });
+  };
+
   componentWillMount() {
     this.updateDimensions();
+    this.updatePhoto();
   }
   componentDidMount() {
     window.addEventListener("resize", this.updateDimensions);
+    this.timer = setInterval(this.updatePhoto, 10000);
   }
   componentWillUnmount() {
     window.removeEventListener("resize", this.updateDimensions);
+    delete this.timer;
   }
   render() {
+    const { photo } = this.state;
     return (
       <Card>
-        <Photo uri="https://avatars0.githubusercontent.com/u/9029281?v=3&s=40" />
+        <Photo src={photo} />
       </Card>
     );
   }
