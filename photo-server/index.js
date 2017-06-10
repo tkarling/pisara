cfbf3fe1320c785390359e03275b4882e88024b7
Antti Myllykoski <amyllykoski@yahoo.com>
2017-06-10 15:58:00 -0700

    Fixed cors.

--- photo-server/index.js
+++ photo-server/index.js
@@ -1,13 +1,15 @@
 'use strict'
 
-var express = require('express')
-var cors = require('cors')
-var fs = require('fs');
-var path = require('path');
-var gm = require('gm')
-var bodyParser = require('body-parser');
-var photoService = require('./PhotoService')
-var app = express()
+const express = require('express')
+const cors = require('cors')
+const fs = require('fs');
+const path = require('path');
+const gm = require('gm')
+const bodyParser = require('body-parser');
+const photoService = require('./PhotoService')
+const app = express()
+const fileDir = path.join(__dirname, 'files');
+const uploadDir = path.join(__dirname, 'uploads');
 
 const SERVER_PORT = 8080;
 
@@ -25,11 +27,20 @@ const mime = {
 var jpegs = []
 var nextIndex = 0
 
-app.use(cors())
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-app.use(bodyParser.json());
+const initUses = () => {
+  app.use(function (req, res, next) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next()
+  })
+  app.use(cors())
+  app.use(bodyParser.urlencoded({
+    extended: true
+  }))
+  app.use(bodyParser.json())
+}
+
+initUses()
 
 app.get('/v2/photo/next', (req, res) => {
   photoService.next(req, res)
@@ -38,6 +49,18 @@ app.get('/v2/photo/next', (req, res) => {
 app.get('/v2/photo/photo', (req, res) => {
   photoService.photo(req, res)
 })
+
+app.post('/v2/photo/upload', function (req, res) {
+  if (!req.files)
+    return res.status(400).send('No files were uploaded.');
+  res.end()
+});
+ 
+app.post('/v2/photo/upload1', function (req, res) {
+  console.log(req)
+  res.header("Content-Type", "application/json")
+  res.send({"Status": "OK"})
+});
 
 var server = app.listen(SERVER_PORT, '0.0.0.0', function () {
   console.log('Server running at http://localhost:' + SERVER_PORT)
