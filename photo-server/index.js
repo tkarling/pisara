ea14b16f85421cedcdca461a7238de9f85b52206
Antti Myllykoski <amyllykoski@yahoo.com>
2017-06-11 16:01:11 -0700

    Added delete endpoint.

--- photo-server/index.js
+++ photo-server/index.js
@@ -2,42 +2,28 @@
 
 const express = require('express')
 const cors = require('cors')
-const fs = require('fs');
-const path = require('path');
+const fs = require('fs')
+const path = require('path')
 const gm = require('gm')
-const bodyParser = require('body-parser');
-const Multer = require('multer');
+const bodyParser = require('body-parser')
+const Multer = require('multer')
 const photoService = require('./PhotoService')
 const app = express()
-const fileDir = path.join('/Users/antti/Pictures/Uploads/', 'Saturday-June-10-2017') //path.join(__dirname, 'files');
+const rootDir = path.join('/Users/antti/Pictures/Uploads/', 'Saturday-June-10-2017') //path.join(__dirname, 'files')
 
-const SERVER_PORT = 8080;
+const SERVER_PORT = 8080
 
 const multer = Multer({
   storage: Multer.MemoryStorage,
   fileSize: 5 * 1024 * 1024
-});
+})
 
-const mime = {
-  html: 'text/html',
-  txt: 'text/plain',
-  css: 'text/css',
-  gif: 'image/gif',
-  jpg: 'image/jpeg',
-  png: 'image/png',
-  svg: 'image/svg+xml',
-  js: 'application/javascript'
-};
-
-var jpegs = []
-var nextIndex = 0
-
-photoService.setRootDir(fileDir)
+photoService.scanImages(rootDir)
 
 const initUses = () => {
   app.use(function (req, res, next) {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
     next()
   })
   app.use(cors())
@@ -57,10 +43,14 @@ app.get('/v2/photo/photo', (req, res) => {
   photoService.photo(req, res)
 })
 
+app.delete('/v2/photo/:fileName', (req, res) => {
+  photoService.delete(rootDir, req, res)
+})
+
 app.post('/v2/photo/upload', multer.single('image'), function (req, res, next) {
-  const data = req.body;
+  const data = req.body
   if (req.file) {
-    let newImage = fileDir + '/' + req.file.originalname
+    let newImage = rootDir + '/' + req.file.originalname
     fs.writeFile(newImage, req.file.buffer, 'binary', function (err) {
       if (err) throw err
       photoService.addImage(newImage)
@@ -68,7 +58,7 @@ app.post('/v2/photo/upload', multer.single('image'), function (req, res, next) {
   }
   res.header("Content-Type", "application/json")
   res.send({ "Status": "OK" })
-});
+})
 
 var server = app.listen(SERVER_PORT, '0.0.0.0', function () {
   console.log('Server running at http://localhost:' + SERVER_PORT)
