a777a8adb4419686556d7cc2bfce76a11781a588
Antti Myllykoski <amyllykoski@yahoo.com>
2017-06-30 10:24:35 -0700

    Integrated photo service to the app. First attempt.

--- photo-server/photo-server-app/public/electron.js
+++ photo-server/photo-server-app/public/electron.js
@@ -5,13 +5,15 @@ const BrowserWindow = electron.BrowserWindow;
 const path = require('path');
 const url = require('url');
 const isDev = require('electron-is-dev');
+const server = require('../../index.js');
 
 let mainWindow;
 
 function createWindow() {
-  mainWindow = new BrowserWindow({width: 900, height: 680});
-  mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
+  mainWindow = new BrowserWindow({ width: 900, height: 680 });
+  mainWindow.loadURL(isDev ? 'http://localhost:3001' : `file://${path.join(__dirname, '../build/index.html')}`);
   mainWindow.on('closed', () => mainWindow = null);
+  mainWindow.webContents.openDevTools();
 }
 
 app.on('ready', createWindow);
